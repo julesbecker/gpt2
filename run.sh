63535299a6656e6339068dc49fdcc9e4ddce2110
Jules <jules_becker@brown.edu>
2020-04-20 14:40:47 -0400

    added run file

---
+++ run.sh
@@ -0,0 +1,10 @@
+pip install gpt-2-simple
+gpt_2_simple finetune --run_name 'score_review_355' \
+    --dataset '/storage/data/score_review.csv' \
+    --checkpoint_dir '/storage/checkpoint' \
+    --model_name '355M' \
+    --model_dir '/storage/models' \
+    --steps 30000 \
+    --restore_from 'latest' \
+    --print_every 20 \
+    --save_every 500

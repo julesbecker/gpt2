106314b198ea2262b4e851f6346d127ad825cdd2
Jules <jules_becker@brown.edu>
2020-04-22 11:04:05 -0400

    added generate script

---
+++ generate.sh
@@ -0,0 +1,8 @@
+pip install gpt-2-simple
+gpt_2_simple generate --run_name 'score_review_355' \
+    --checkpoint_dir '/storage/checkpoint' \
+    --folder '/artifacts' \
+    --nsamples 100 \
+    --batch_size 10 \
+    --prefix '<|startoftext|>' \
+    --truncate '<|endoftext|>'

67b9d6e997721038d20060cc69d6ce018d2714d9
Jules <jules_becker@brown.edu>
2020-04-22 11:08:29 -0400

    needed to specify nonexistent folder

--- generate.sh
+++ generate.sh
@@ -1,7 +1,7 @@
 pip install gpt-2-simple
 gpt_2_simple generate --run_name 'score_review_355' \
     --checkpoint_dir '/storage/checkpoint' \
-    --folder '/artifacts' \
+    --folder '/artifacts/output' \
     --nsamples 100 \
     --batch_size 10 \
     --prefix '<|startoftext|>' \

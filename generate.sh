ac8da03c7da56145f688f411753c986acb8ca4d5
Jules <jules_becker@brown.edu>
2020-04-23 13:50:54 -0400

    changed generate for artforum

--- generate.sh
+++ generate.sh
@@ -1,8 +1,8 @@
 pip install gpt-2-simple
-gpt_2_simple generate --run_name 'score_review_355' \
+gpt_2_simple generate --run_name 'artforum_text_355' \
     --checkpoint_dir '/storage/checkpoint' \
     --folder '/artifacts/output' \
-    --nsamples 100 \
+    --nsamples 200 \
     --batch_size 10 \
     --prefix '<|startoftext|>' \
     --truncate '<|endoftext|>'

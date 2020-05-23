952c201bbe892f7558a72b3587121b6e56adcf49
julesbecker <becker2430@gmail.com>
2020-05-23 16:00:49 -0400

    update run artforum for reviews

--- run_artforum.sh
+++ run_artforum.sh
@@ -1,10 +1,10 @@
 pip install gpt-2-simple
-gpt_2_simple finetune --run_name 'artforum_text_355' \
-    --dataset '/storage/data/artforum_shorter.csv' \
+gpt_2_simple finetune --run_name 'artforum_reviews_355' \
+    --dataset '/storage/data/artforum_reviews.csv' \
     --checkpoint_dir '/storage/checkpoint' \
     --model_name '355M' \
     --model_dir '/storage/models' \
-    --steps 45000 \
+    --steps 50000 \
     --restore_from 'latest' \
     --print_every 20 \
-    --save_every 500
+    --save_every 1000

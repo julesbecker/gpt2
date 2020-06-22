6c57c010c46fe2bd2a6704427f222be7024831f6
julesbecker <becker2430@gmail.com>
2020-06-22 09:04:58 -0400

    make the pip install quiet, change model name

--- run.sh
+++ run.sh
@@ -1,10 +1,10 @@
-pip install gpt-2-simple
-gpt_2_simple finetune --run_name 'score_review_355' \
-    --dataset '/storage/data/score_review.csv' \
+pip install -q gpt-2-simple
+gpt_2_simple finetune --run_name 'artforum_reviews_355' \
+    --dataset '/storage/data/artforum_reviews.csv' \
     --checkpoint_dir '/storage/checkpoint' \
     --model_name '355M' \
     --model_dir '/storage/models' \
-    --steps 36000 \
+    --steps 50000 \
     --restore_from 'latest' \
     --print_every 20 \
-    --save_every 500
+    --save_every 1000
